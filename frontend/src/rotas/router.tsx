072c884a502bccbf8a67d5bc6741316109b3bd53
Hugo Lamounier <hugolamo@gmail.com>
2024-08-12 13:42:07 -0300

    Fixes

--- frontend/src/rotas/router.tsx
+++ frontend/src/rotas/router.tsx
@@ -1,15 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
-import Index from "../paginas";
 import CriarUsuario from "../paginas/Usuario/Criar";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Index />,
-    index: true,
-  },
-  {
-    path: "usuario/criar",
     element: <CriarUsuario />,
+    index: true,
   },
 ]);
